347f2e143d43245e32f8142e8a6dc2bdef6e6190
jodyneckles <jody@technative.tv>
2018-11-21 18:09:51 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+class Dog
+# Class names begin with capital letters because they are stored in Ruby constants.
+# If your class name contains two words, the name should be CamelCased, like this: class MyClass
+
+
+end
+
+
+# instantiate three new dog.
+fido = Dog.new
+
+snoopy = Dog.new
+
+lassie = Dog.new
+# snoopy and fido are two different variables pointing at separate instances of the Dog class.
+
+
+class Person
+
+end
+
+
+adele_goldberg = Person.new
+
+alan_kay = Person.new
